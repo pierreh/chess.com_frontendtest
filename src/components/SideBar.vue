5a86ecd2dfc198231b2230e05f67ca7cb4d7db1d
pierreh <pierrehavelaar@gmail.com>
2023-04-26 10:38:09 +0100

    Wrap sidebar output in columns

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -6,9 +6,16 @@ import State from './State';
 <template>
     <h1>Chess.com Frontend Challenge</h1>
     <div v-show="State.clickedSquares.length > 0">Squares clicked:</div>
-    <div v-for="(square, idx) in State.clickedSquares" :key="idx">{{ idx }}: {{ square }}</div>
+    <div class="container">
+        <span v-for="(square, idx) in State.clickedSquares" :key="idx">{{ idx }}: {{ square }}</span>
+    </div>
 </template>
 
 <style scoped>
-
+.container {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    height: 30vw;
+}
 </style>
